04682a79133228a21cae14af60185ec65743461a
YuriyDub <yuriyyuriovich@gmail.com>
2023-07-11 23:22:06 +0300

    add redirect to about page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Header } from '../Header';
+import { useEffect } from 'react';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => navigate('/about'), [navigate]);
+
   return (
     <>
       <Header />
